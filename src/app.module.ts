3b1ae8acdef1f3abed4fb01808b1a61cfe011292
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-06-16 10:59:32 -0500

    fix(main): complete configuration of the sql server connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MangaModule } from './manga/manga.module';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mssql',
-      host: 'localhost'
-    })
+      host: 'localhost',
+      port: 1433,
+      username: 'user',
+      password: 'password',
+      database: 'MangaDB',
+      entities: [__dirname + '/**/*.entity{.ts, .js}'],
+      synchronize: true,
+      options: {
+        encrypt: false
+      }
+    }),
+    MangaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
