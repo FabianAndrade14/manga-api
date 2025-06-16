6e43079c33e3bf9b92278a5076cafab16040cc10
Fabian Andrade <40735444+FabianAndrade14@users.noreply.github.com>
2025-06-16 10:10:11 -0500

    fix(main): fixing the sql connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Manga API')
+    .setDescription('API de tomos de My Hero Academia y Demon Slayer')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+  // await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
